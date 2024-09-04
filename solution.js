516d6e0e78e1a98961b65a66a1a94c0acca81566
Akash new <akashladwa64.in>
2024-09-04 18:17:49 +0530

    Problem2 Added

--- solution.js
+++ solution.js
@@ -10,4 +10,17 @@ function problem1(){
     }
     return emails;
 }
-module.exports={problem1}
+
+// Implement a function that retrieves and prints the hobbies of individuals with a specific age, say 30 years old.
+
+function problem2(age){
+  let hobbies=[]
+  for(let i =0 ; i < person.length ; i++){
+      if(person[i].age===age){
+          hobbies.push(person[i].hobbies)
+      }
+  }
+  return hobbies;
+}
+
+module.exports={problem1,problem2}
--- test.js
+++ test.js
@@ -5,3 +5,7 @@ let newemails=problem1(person)
 console.log(newemails)
 
 
+//problem2 testcase
+let newhobbies=problem2(30)
+console.log(newhobbies);
+
